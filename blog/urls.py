762ef137326d0de488157a2b4012a9b44df4f867
FalseHuman <emilkhazioff@yandex.com>
2021-02-28 11:00:17 +0500

    Глава 3, готовый блог

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
+
 urlpatterns = [
  # post views
  path('', views.post_list, name='post_list'),
@@ -8,4 +11,6 @@ urlpatterns = [
  path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
  path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
  path('<int:post_id>/share/', views.post_share, name='post_share'),
+ path('feed/', LatestPostsFeed(), name='post_feed'),
+ path('search/', views.post_search, name='post_search'),
 ]
