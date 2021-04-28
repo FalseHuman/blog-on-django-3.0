91d976e4bfd3ee29352d7c2c9e58cfcc0080de1e
Emil <emilkhazioff@yandex.com>
2021-04-28 20:05:18 +0500

    update e-mail

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'emilkhazioff@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
