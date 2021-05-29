264f62f84c75a6044d176c8c20ad4b4c9dfab7fd
Emil <emilkhazioff@yandex.com>
2021-05-29 13:52:19 +0500

    Добавил отправку по мылу в настройки приложений

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+    'send_email.apps.SendEmailConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
