e4aadc1a78745fd44955ec4a80fe579025610dc5
Emil <emilkhazioff@yandex.com>
2021-05-29 14:36:48 +0500

    Up email

--- blog/views.py
+++ blog/views.py
@@ -54,7 +54,7 @@ def post_share(request, post_id):
                 post_url = request.build_absolute_uri(post.get_absolute_url())
                 subject = '{} ({}) рекомендует посмотреть "{}"'.format(cd['name'], cd['email'], post.title)
                 message = 'Посмотри "{}" на {}\n\n{} прокомментировал:{}'.format(post.title, post_url, cd['name'], cd['comments'])
-                send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+                send_mail(subject, message, 'emilkhazioff@yandex.ru', [cd['to']])
                 sent = True
      else:
         form = EmailPostForm()
