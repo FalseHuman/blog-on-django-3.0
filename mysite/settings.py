e446e556f1d9095840c05b73bec69fe5da469c10
Emil <emilkhazioff@yandex.com>
2021-05-29 14:32:19 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
-    'send_email.apps.SendEmailConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
